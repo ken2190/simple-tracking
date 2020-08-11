9490dfc4c47231967111a0c6fc45a0b5b752ebea
ramondfdez <dsrmaniaco@gmail.com>
2020-08-11 11:40:24 +0200

    Revert "Update model.py"
    This reverts commit 534867c0357ded7342864b9f3ab28ebd3671c9f3.

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -947,7 +947,7 @@ def fpn_classifier_graph(rois, feature_maps, image_meta,
                            name='mrcnn_bbox_fc')(shared)
     # Reshape to [batch, num_rois, NUM_CLASSES, (dy, dx, log(dh), log(dw))]
     s = K.int_shape(x)
-    mrcnn_bbox = KL.Reshape((-1, num_classes, 4), name="mrcnn_bbox")(x)
+    mrcnn_bbox = KL.Reshape((s[1], num_classes, 4), name="mrcnn_bbox")(x)
 
     return mrcnn_class_logits, mrcnn_probs, mrcnn_bbox
 
